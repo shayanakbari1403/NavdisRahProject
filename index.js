066e9bfe9a3c62519ea2592b84c97d56277032fe
shayanakbari1403 <shayanakbari1403@gmail.com>
2025-07-12 18:48:15 +0330

    Update index.js toggleLabel function added to helper functions

--- index.js
+++ index.js
@@ -95,6 +95,13 @@ app.engine(
       isEqual: function (arg1, arg2, options) {
         return arg1 == arg2 ? options.fn(this) : options.inverse(this);
       },
+      toggleLabel: function(lang) {
+	if(lang === 'fa') {
+		return 'منو';
+	} else {
+		return 'Menu';
+	}
+      },
     },
   })
 );
