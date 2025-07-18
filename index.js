77525fa1f4cd395f616e984b0fdbd0dec2f86998
shayanakbari1403 <shayanakbari1403@gmail.com>
2025-07-18 19:31:24 +0330

    Update index.js project categories routing III

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ app.get("/fa/projects/:category?", (req, res) => {
 });
 app.get("/en/projects/:category?", (req, res) => {
   const selectedCategory = req.params.category?.toLowerCase() || "all";
-
+  const lang = "en";
   const docs = collection.find(item => item.name === "projects" && item.lang === "en");
   if (!docs) {
     console.error("No matching record found");
@@ -283,7 +283,8 @@ app.get("/en/projects/:category?", (req, res) => {
   const model = {
     ...docs,
     list: projectsList,
-    selectedCategory
+    selectedCategory: category,
+    lang
   };
 
   res.render("home/projects", model);
